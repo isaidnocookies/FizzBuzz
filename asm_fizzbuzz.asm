23b9fcfbbf15e283f49b059d274db5372cf6798d
Chris <isaidnocookies@gmail.com>
2019-03-14 12:47:20 -0700

    Working on GOD DAMN PRINTING NUMBERS

--- asm_fizzbuzz.asm
+++ asm_fizzbuzz.asm
@@ -4,10 +4,9 @@ section .text
 
 _start:
 
-  mov rdi, 3
-  neg rdi
-
-  mov rsi, 0
+  mov rdi, 5                    ; Start of FizzBuzz range
+  neg rdi                       ; Negate the start of the range
+  mov rsi, 5                    ; End of the FizzBuzz range
 
   call fizzBuzz
 
@@ -26,6 +25,10 @@ fizzBuzz:
   mov rdx, 0
   mov rax, rdi
   mov rbx, 15
+  cmp rax, 0                    ; check rax and get the abs
+  jge continueFizzBuzzCheck
+  neg rax
+continueFizzBuzzCheck:
   idiv rbx
   cmp rdx, 0
   je  printFizzBuzz
@@ -34,6 +37,10 @@ fizzBuzz:
   mov rdx, 0
   mov rax, rdi
   mov rbx, 3
+  cmp rax, 0                    ; check rax and get the abs
+  jge continueFizzCheck
+  neg rax
+continueFizzCheck:
   idiv rbx
   cmp rdx, 0
   je printFizz
@@ -42,10 +49,13 @@ fizzBuzz:
   mov rdx, 0
   mov rax, rdi
   mov rbx, 5
+  cmp rax, 0                    ; check rax and get the abs
+  jge continueBuzzCheck
+  neg rax
+continueBuzzCheck:
   idiv rbx
   cmp rdx, 0
   je printBuzz
-
   jmp printIndex
 
 fizzBuzzLoopEnd:
@@ -56,13 +66,21 @@ finishFizzBuzz:
   ret
 
 printIndex:
-  mov     rdx,mynumLength    
-  mov     rcx,mynum          
+
+  mov     [myNumber], rax
+  mov     rcx, myNumber
+  mov     rdx, 1       
   mov     rbx,1                 ; file handle (stdout)
   mov     rax,4                 ; sys_write
   int     0x80
-  jmp fizzBuzzLoopEnd
 
+  mov     rdx,newlineLength    
+  mov     rcx,newline     
+  mov     rbx,1                 ; file handle (stdout)
+  mov     rax,4                 ; sys_write
+  int     0x80
+
+  jmp fizzBuzzLoopEnd
 
 
 
@@ -96,17 +114,22 @@ printBuzz:
 
 
 
-
 section .data
 
-fizz            db 'Fizz',0xa
-fizzLength      equ $ - fizz
+  fizz            db 'Fizz',0xa
+  fizzLength      equ $ - fizz
 
-buzz            db 'Buzz',0xa
-buzzLength      equ $ - buzz
+  buzz            db 'Buzz',0xa
+  buzzLength      equ $ - buzz
 
-fizzbuzz        db 'FizzBuzz',0xa
-fizzbuzzLength  equ $ - fizzbuzz
+  fizzbuzz        db 'FizzBuzz',0xa
+  fizzbuzzLength  equ $ - fizzbuzz
 
-mynum           db '[number]',0xa
-mynumLength     equ $ - mynum
+  mynum           db '[number]',0xa
+  mynumLength     equ $ - mynum
+
+  newline         db '',0xa
+  newlineLength   equ $ - newline
+
+section .bss
+  myNumber resb 1
