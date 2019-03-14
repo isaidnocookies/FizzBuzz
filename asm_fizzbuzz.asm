60bcb26c428de6fb76a8606b17f1951f5b30c49b
Chris <isaidnocookies@gmail.com>
2019-03-14 16:48:40 -0700

    Updated to fix zero

--- asm_fizzbuzz.asm
+++ asm_fizzbuzz.asm
@@ -4,22 +4,27 @@ section .text
 
 _start:
 
-  mov rdi, 5                    ; Start of FizzBuzz range
+  mov rdi, 3                    ; Start of FizzBuzz range
   neg rdi                       ; Negate the start of the range
-  mov rsi, 5                    ; End of the FizzBuzz range
+  mov rsi, 15                    ; End of the FizzBuzz range
 
-  call fizzBuzz
+  call fizzBuzz                 ; You know...
 
   mov     rbx,0                 ; exit code - successful
   mov     rax,1                 ; sys_exit
   int     0x80
+
+
 
 fizzBuzz:
   cmp rdi, rsi
   jg finishFizzBuzz
 
   cmp rdi, 0
-  je printIndex
+  jne continueFizzBuzz
+  call printIndex
+  jmp fizzBuzzLoopEnd
+continueFizzBuzz:  
 
   ; Print FizzBuzz
   mov rdx, 0
@@ -56,7 +61,7 @@ continueBuzzCheck:
   idiv rbx
   cmp rdx, 0
   je printBuzz
-  jmp printIndex
+  call printIndex
 
 fizzBuzzLoopEnd:
   inc rdi
@@ -65,10 +70,7 @@ fizzBuzzLoopEnd:
 finishFizzBuzz:
   ret
 
-
-
 printFizzBuzz:
-
   mov     edx,fizzbuzzLength    
   mov     ecx,fizzbuzz          
   mov     ebx,1                 ; file handle (stdout)
@@ -76,10 +78,7 @@ printFizzBuzz:
   int     0x80
   jmp fizzBuzzLoopEnd
 
-
-
 printFizz:
-
   mov     edx,fizzLength    
   mov     ecx,fizz
   mov     ebx,1                 ; file handle (stdout)
@@ -87,10 +86,7 @@ printFizz:
   int     0x80
   jmp fizzBuzzLoopEnd
 
-
-
 printBuzz:
-
   mov     edx,buzzLength    
   mov     ecx,buzz          
   mov     ebx,1                 ; file handle (stdout)
@@ -99,15 +95,69 @@ printBuzz:
   jmp fizzBuzzLoopEnd
 
 
-
 printIndex:
+  push    rdi
+  push    rax
+  push    rsi
 
-  mov     [myNumber], rax
-  mov     rcx, myNumber
-  mov     rdx, 1       
+  cmp rdi, 0
+  jg printIndexLoop
+  cmp rdi, 0
+  je printZero
+  neg rdi
+  mov     [index], rdi
+  mov     rcx, minusSign        ; print '-' with the sys_write
+  mov     rdx,1
   mov     rbx,1                 ; file handle (stdout)
   mov     rax,4                 ; sys_write
   int     0x80
+  jmp printIndexLoop
+
+printZero:
+  mov rax, '0'
+  mov [num], rax
+  mov rax, 4
+  mov rbx, 1
+  push rcx
+
+  mov rcx, num        
+  mov rdx, 1        
+  int 0x80
+
+  add rsp, 8
+  jmp endPrintIndexLoop
+
+
+printIndexLoop:
+  mov rax, '1'
+  mov [num], rax
+  mov rax, 4
+  mov rbx, 1
+  push rcx
+
+  mov rcx, num        
+  mov rdx, 1        
+  int 0x80
+
+  add rsp, 8
+
+endPrintIndexLoop:
+
+  call printNewline
+  pop rsi
+  pop rax
+  pop rdi
+  
+  ret
+
+
+
+
+printNewline:
+
+  push    rdx
+  push    rcx
+  push    rax
 
   mov     rdx,newlineLength    
   mov     rcx,newline     
@@ -115,25 +165,31 @@ printIndex:
   mov     rax,4                 ; sys_write
   int     0x80
 
-  jmp fizzBuzzLoopEnd
+  pop     rax
+  pop     rcx
+  pop     rdx
+  
+  ret
 
 
 section .data
 
-  fizz            db 'Fizz',0xa
-  fizzLength      equ $ - fizz
+  fizz              db 'Fizz',0xa
+  fizzLength        equ $ - fizz
 
-  buzz            db 'Buzz',0xa
-  buzzLength      equ $ - buzz
+  buzz              db 'Buzz',0xa
+  buzzLength        equ $ - buzz
 
-  fizzbuzz        db 'FizzBuzz',0xa
-  fizzbuzzLength  equ $ - fizzbuzz
+  fizzbuzz          db 'FizzBuzz',0xa
+  fizzbuzzLength    equ $ - fizzbuzz
 
-  mynum           db '[number]',0xa
-  mynumLength     equ $ - mynum
+  newline           db '',0xa
+  newlineLength     equ $ - newline
 
-  newline         db '',0xa
-  newlineLength   equ $ - newline
+  largestMagnitude  equ 10000
+  minusSign         db '-'
 
 section .bss
-  myNumber resb 1
+  index resb 1
+  lMag  resb 1
+  num   resb 1
